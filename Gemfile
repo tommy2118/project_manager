9c3b51ad2337db96415760b606ed7b68fdcf7730
Tommy A. Caruso <tommy.caruso2118@gmail.com>
2015-12-16 22:36:18 -0500

    Add ability to create a new project

--- Gemfile
+++ Gemfile
@@ -1,20 +1,22 @@
 source "https://rubygems.org"
 
 
-gem "rails", "4.2.4"
-gem "pg"
-gem "sass-rails", "~> 5.0"
-gem "uglifier", ">= 1.3.0"
-gem "coffee-rails", "~> 4.1.0"
-gem "jquery-rails"
-gem "turbolinks"
-gem "jbuilder", "~> 2.0"
-gem "sdoc", "~> 0.4.0", group: :doc
 gem "bcrypt", "~> 3.1.7"
+gem "coffee-rails", "~> 4.1.0"
+gem "devise"
+gem "jbuilder", "~> 2.0"
+gem "jquery-rails"
+gem "pg"
 gem "puma"
+gem "rails", "4.2.4"
+gem "sass-rails", "~> 5.0"
+gem "sdoc", "~> 0.4.0", group: :doc
+gem "turbolinks"
+gem "uglifier", ">= 1.3.0"
 
 group :development, :test do
   gem "byebug"
+  gem "capybara"
   gem "rspec-rails", "~> 3.0"
   gem "shoulda-matchers", "~> 3.0"
 end
---
+++ spec/features/create_user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "users can create user account" do
+  scenario "with valid input" do
+
+    visit "/"
+    click_link "Create Account"
+    fill_in "Name", with: "Tommy Caruso"
+    fill_in "Email", with: "user@test.com"
+    fill_in "user_password", with: "password"
+    fill_in "Password Confirmation", with: "password"
+    fill_in "Phone Number", with: "(555) 555-1234"
+    fill_in "Street Address", with: "123 Park Ave"
+    fill_in "City", with: "Corona"
+    fill_in "State", with: "CA"
+    fill_in "Zip Code", with: "90210"
+    click_button "Create Account"
+    expect(page).to have_content("You have successfully created an account.")
+  end
+end
